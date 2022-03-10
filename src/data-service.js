a010abb3211a743e607543f435e436fe05a77ef4
Blanca Ley <blanca.ley@outlook.com>
2022-03-10 22:08:27 +0100

    Refactor getData into a reusable function for any URL

--- src/data-service.js
+++ src/data-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const getData = async () => {
-  const response = await axios.get("https://swapi.dev/api/people");
-  console.log(response);
+export const getData = async (url) => {
+  const response = await axios.get(url);
+  return response.data;
 }
