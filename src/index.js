525e88eb2bc3ad0b5b410545619e9ad93c199e50
Blanca Ley <blanca.ley@outlook.com>
2022-03-11 10:37:05 +0100

    Show compare button

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { getData } from './data-service'
 
+// Variables
+const compareBtn = document.getElementById("compareBtn");
+
 const getCharacterByID = async (id) => {
   if(!id) {
     throw new Error("ID was not provided");
@@ -11,3 +14,33 @@ const getAllCharacters = async () => {
   const allCharacters = await getData(`https://swapi.dev/api/people`);
   return allCharacters.results;
 }
+
+// const createCharacter = async () => {
+//   // Get selected characters name
+//   const mainCharacterName = document.getElementById("mainCharacter").value;
+//   const secondaryCharacterName = document.getElementById("secondaryCharacter").value;
+  
+//   // Get character data from API for selected names
+//   const allCharacters = await getAllCharacters();
+//   const mainCharacter = allCharacters.find(char => char.name === mainCharacterName);
+//   const secondaryCharacter = allCharacters.find(char => char.name === secondaryCharacterName);
+
+//   // Create Character object. 
+//   const newCharacter = new Character(mainCharacter, secondaryCharacter);
+//   return newCharacter;
+// }
+
+compareBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  createCharacter();
+})
+// class Character {
+//   constructor(name) {
+//     this.name = name,
+//     this.gender =,
+//     this.height =,
+//     this.mass =,
+//     this.hairColor =,
+//     this.picURL =
+//   }
+// }
