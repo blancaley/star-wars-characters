6f20278dc97da78bbb8750da38a642fd9081c94e
Blanca Ley <blanca.ley@outlook.com>
2022-03-11 10:41:09 +0100

    Add function to create character object from selected characters

--- src/index.js
+++ src/index.js
@@ -15,20 +15,20 @@ const getAllCharacters = async () => {
   return allCharacters.results;
 }
 
-// const createCharacter = async () => {
-//   // Get selected characters name
-//   const mainCharacterName = document.getElementById("mainCharacter").value;
-//   const secondaryCharacterName = document.getElementById("secondaryCharacter").value;
+const createCharacter = async () => {
+  // Get selected characters name
+  const mainCharacterName = document.getElementById("mainCharacter").value;
+  const secondaryCharacterName = document.getElementById("secondaryCharacter").value;
   
-//   // Get character data from API for selected names
-//   const allCharacters = await getAllCharacters();
-//   const mainCharacter = allCharacters.find(char => char.name === mainCharacterName);
-//   const secondaryCharacter = allCharacters.find(char => char.name === secondaryCharacterName);
+  // Get character data from API for selected names
+  const allCharacters = await getAllCharacters();
+  const mainCharacter = allCharacters.find(char => char.name === mainCharacterName);
+  const secondaryCharacter = allCharacters.find(char => char.name === secondaryCharacterName);
 
-//   // Create Character object. 
-//   const newCharacter = new Character(mainCharacter, secondaryCharacter);
-//   return newCharacter;
-// }
+  // Create Character object. 
+  const newCharacter = new Character(mainCharacter, secondaryCharacter);
+  return newCharacter;
+}
 
 compareBtn.addEventListener("click", (e) => {
   e.preventDefault();
