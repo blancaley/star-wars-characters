c7ea31cb86ea3f8396fe61a9a1fb3f185ad80232
Blanca Ley <blanca.ley@outlook.com>
2022-03-11 10:47:52 +0100

    Add Character class with constructor method

--- src/index.js
+++ src/index.js
@@ -14,6 +14,16 @@ const getAllCharacters = async () => {
   const allCharacters = await getData(`https://swapi.dev/api/people`);
   return allCharacters.results;
 }
+class Character {
+  constructor(mainCharacter, secondaryCharacter) {
+    this.name = mainCharacter.name,
+    this.gender = mainCharacter.gender,
+    this.height = mainCharacter.height,
+    this.mass = mainCharacter.mass,
+    this.hairColor = mainCharacter.hair_color,
+    this.picURL = ""
+  }
+}
 
 const createCharacter = async () => {
   // Get selected characters name
@@ -34,13 +44,3 @@ compareBtn.addEventListener("click", (e) => {
   e.preventDefault();
   createCharacter();
 })
-// class Character {
-//   constructor(name) {
-//     this.name = name,
-//     this.gender =,
-//     this.height =,
-//     this.mass =,
-//     this.hairColor =,
-//     this.picURL =
-//   }
-// }
